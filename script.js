45c4b04b1e54a9a008e5df6f2d47dd35bb3d6ee3
CnYeShneg <55972943+CnYeSheng@users.noreply.github.com>
2024-04-27 19:23:51 +0800

    dark

--- script.js
+++ script.js
@@ -293,3 +293,36 @@ window.onload = function() {
         lockoutTime = parseInt(savedLockoutTime);
     }
 };
+
+
+
+// 在網頁載入完成後執行
+window.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+  
+    // 檢查瀏覽器是否有儲存深色模式設定
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  
+    // 根據儲存設定切換深色模式
+    if (isDarkMode) {
+      body.classList.add('dark-mode');
+      darkModeToggle.textContent = '🌓';
+    }
+  
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        const isDarkMode = body.classList.contains('dark-mode');
+        darkModeToggle.textContent = isDarkMode ? '🌓' : '🌓';
+        localStorage.setItem('darkMode', isDarkMode);
+      
+        // 如果切換為深色模式,則在動畫結束後再更新背景色
+        if (isDarkMode) {
+          setTimeout(() => {
+            body.style.backgroundColor = '#222';
+          }, 500); // 動畫時間為 0.5 秒
+        } else {
+          body.style.backgroundColor = '#f8f9fa'; // 直接更新為淺色背景
+        }
+      });
+  });
